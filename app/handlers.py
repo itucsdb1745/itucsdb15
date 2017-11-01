ccbdbd6b17057a75fc31d74f3c140778575fb051
Yunus Güngör <yunusgnr@gmail.com>
2017-11-01 03:47:01 +0300

    Added ability to add messages

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request, redirect, url_for
+from app.models.message import Message
 
 site = Blueprint('site', __name__)
 
-@site.route('/')
+@site.route('/', methods=['GET', 'POST'])
 def home_page():
+    if request.method == 'GET':
+        messages=current_app.messageStore.get_messages()
+        return render_template('home.html', messages=sorted(messages.items()))
+    elif request.form['messageTitle']:
+        title=request.form['messageTitle']
+        text=request.form['messageText']
+        #Add new message
+        current_app.messageStore.add_message(Message(title,text))
+        return redirect(url_for('site.home_page'))
     messages=current_app.messageStore.get_messages()
     return render_template('home.html', messages=sorted(messages.items()))
 
