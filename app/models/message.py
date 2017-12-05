596e33f7b64e3e45959ee627dffa75dcc03ad624
Yunus Güngör <yunusgnr@gmail.com>
2017-12-05 12:06:16 +0300

    message object has been changed according to databse structure

--- app/models/message.py
+++ app/models/message.py
@@ -1,6 +1,6 @@
 class Message:
-    def __init__(self, title, text=None, id=None):
+    def __init__(self, title, text=None, id=None, username='admin'):
         self.title = title
         self.text = text
         self.id = id
-        
+        self.username = username
